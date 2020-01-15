908c775ef319609a0776517956f2bc588a0445c5
honoryin <honoryin@tencent.com>
2020-01-15 17:55:44 +0800

    fix:命名冲突

--- controllers/wraps.py
+++ controllers/wraps.py
@@ -8,8 +8,8 @@ def admin_pwd(f):
     @wraps(f)
     async def _run(request, *args):
         if srvconf.admin_pwd:
-            admin_pwd = get_req_key(request, "admin_pwd", "")
-            if admin_pwd != srvconf.admin_pwd:
+            pwd = get_req_key(request, "admin_pwd", "")
+            if pwd != srvconf.admin_pwd:
                 return json({"err": 1, "err_msg": "pwd is not correct"})
         rsp = await f(request)
         return rsp
