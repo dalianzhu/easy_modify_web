c54207fa9249e520f62df5cc84ed6a50d7b05c2a
honoryin <honoryin@tencent.com>
2020-01-15 16:39:54 +0800

    feat: 修改代码，完全依赖redis

--- handlers/add_handler.py
+++ handlers/add_handler.py
@@ -8,6 +8,7 @@ from utils.http_helper import get_req_key
 from resources import redis_set, redis_get
 import datetime
 
+
 async def add_handler(request):
     # http://localhost:8000/add?name=dynamic_hello&func=dynamic_hello&code=xxx&web_path=xxx
     name = get_req_key(request, "name")
@@ -15,18 +16,18 @@ async def add_handler(request):
     code = get_req_key(request, "code")
     web_path = get_req_key(request, "web_path")
 
-    c_add_handler(request.app, name, func, code, web_path)
+    # c_add_handler(request.app, name, func, code, web_path)
 
     db_handler_list = await redis_get("db_handler_list")
     if name not in db_handler_list:
         handler_info = {
-                           "name": name,
-                           "file_path": "",
-                           "func": func,
-                           "web_path": web_path,
-                           "code": code,
-                            "update_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                       }
+            "name": name,
+            "file_path": "",
+            "func": func,
+            "web_path": web_path,
+            "code": code,
+            "update_time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
         db_handler_list[name] = handler_info
     else:
         db_handler_list[name]['code'] = code
